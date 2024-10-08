c546457e154c342ffa54e4d2ba131cc8c8de877a
Ralf <hackerralf8@gmail.com>
2024-10-08 23:19:34 +0300

    Agents: init beacon

---
+++ Agents/beacon/beacon/beacon.cpp
@@ -0,0 +1,83 @@
+﻿#include <windows.h>
+
+#if defined(BUILD_SVC)
+
+SERVICE_STATUS        ServiceStatus = {0};
+SERVICE_STATUS_HANDLE hStatus;
+
+void ServiceMain(int argc, char** argv);
+void ControlHandler(DWORD request);
+
+void ServiceMain(int argc, char** argv) {
+    hStatus = RegisterServiceCtrlHandler(SERVICE_NAME, (LPHANDLER_FUNCTION)ControlHandler);
+    if( !hStatus )
+        return;
+
+    ServiceStatus.dwServiceType = SERVICE_WIN32;
+    ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+    ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
+    
+    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+    SetServiceStatus(hStatus, &ServiceStatus);
+
+    /// Main
+
+    ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+    SetServiceStatus(hStatus, &ServiceStatus);
+}
+
+void ControlHandler(DWORD request) {
+    switch (request) {
+    case SERVICE_CONTROL_STOP:
+        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+        break;
+    case SERVICE_CONTROL_SHUTDOWN:
+        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+        break;
+    default:
+        break;
+    }
+
+    SetServiceStatus(hStatus, &ServiceStatus);
+}
+
+int MainSvc() {
+    SERVICE_TABLE_ENTRY ServiceTable[2];
+    ServiceTable[0].lpServiceName = (LPWSTR)SERVICE_NAME;
+    ServiceTable[0].lpServiceProc = (LPSERVICE_MAIN_FUNCTION)ServiceMain;
+
+    ServiceTable[1].lpServiceName = NULL;
+    ServiceTable[1].lpServiceProc = NULL;
+
+    StartServiceCtrlDispatcher(ServiceTable);
+
+    return 0;
+}
+
+#elif defined(BUILD_DLL)
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) 
+{
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        /// MAIN
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+#else
+
+int MainExe()
+{
+    /// MAIN
+	return 0;
+}
+
+#endif
