1fa0f1cb969029e15da8f833712f6e763bcf5a9b
Ralf <54005327+RalfHacker@users.noreply.github.com>
2025-01-27 16:16:07 +0300

    Update ssl_gen.sh

--- AdaptixServer/ssl_gen.sh
+++ AdaptixServer/ssl_gen.sh
@@ -1,6 +1,6 @@
 # ECDSA recommendation key ≥ secp384r1
 # List ECDSA the supported curves (openssl ecparam -list_curves)
-openssl req -x509 -nodes -newkey ec:secp384r1 -keyout server.ecdsa.key -out server.ecdsa.crt -days 3650
+# openssl req -x509 -nodes -newkey ec:secp384r1 -keyout server.ecdsa.key -out server.ecdsa.crt -days 3650
 
 # RSA recommendation key ≥ 2048-bit
 openssl req -x509 -nodes -newkey rsa:2048 -keyout server.rsa.key -out server.rsa.crt -days 3650
