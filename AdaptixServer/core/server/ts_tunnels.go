0ffe856ea277830be54186c427579338902da3f1
Ralf <hackerralf8@gmail.com>
2025-02-16 17:42:56 +0300

    Server socks5 start

--- AdaptixServer/core/server/ts_tunnels.go
+++ AdaptixServer/core/server/ts_tunnels.go
@@ -17,13 +17,98 @@ import (
 )
 
 func (ts *Teamserver) TsTunnelStop(TunnelId string) {
-	value, ok := ts.tunnels.Get(TunnelId)
+	value, ok := ts.tunnels.GetDelete(TunnelId)
 	if !ok {
 		return
 	}
 	tunnel, _ := value.(*Tunnel)
 
 	tunnel.listener.Close()
+
+	packet := CreateSpTunnelDelete(tunnel.Data)
+	ts.TsSyncAllClients(packet)
+	ts.events.Put(packet)
+}
+
+/// Socks5
+
+func (ts *Teamserver) TsTunnelStartSocks5(AgentId string, Port int, FuncMsgConnect func(channelId int, addr string, port int) []byte, FuncMsgWrite func(channelId int, data []byte) []byte, FuncMsgClose func(channelId int) []byte) error {
+	var (
+		agent       *Agent
+		socksTunnel *Tunnel
+	)
+
+	value, ok := ts.agents.Get(AgentId)
+	if !ok {
+		return errors.New("agent not found")
+	}
+	agent, _ = value.(*Agent)
+
+	port := strconv.Itoa(Port)
+	addr := "0.0.0.0:" + port
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	socksTunnel = &Tunnel{
+		listener:    listener,
+		connections: safe.NewMap(),
+
+		handlerConnect: FuncMsgConnect,
+		handlerWrite:   FuncMsgWrite,
+		handlerClose:   FuncMsgClose,
+	}
+
+	go func() {
+		for {
+			var conn net.Conn
+			conn, err = socksTunnel.listener.Accept()
+			if err != nil {
+				fmt.Printf("Error socks connect: %v\n", err)
+				return
+			}
+			go handleRequestSocks5(agent, socksTunnel, conn)
+		}
+	}()
+
+	time.Sleep(300 * time.Millisecond)
+
+	if err != nil {
+		return err
+	}
+
+	id := krypt.CRC32([]byte(agent.Data.Id + "socks5" + port))
+	tunnelId := fmt.Sprintf("%08x", id)
+
+	socksTunnel.Data = adaptix.TunnelData{
+		TunnelId: tunnelId,
+		AgentId:  agent.Data.Id,
+		Computer: agent.Data.Computer,
+		Username: agent.Data.Username,
+		Process:  agent.Data.Process,
+		Type:     "SOCKS5 proxy",
+		Info:     "",
+		Port:     port,
+		Client:   "",
+		Fport:    "",
+		Fhost:    "",
+	}
+
+	ts.tunnels.Put(tunnelId, socksTunnel)
+
+	packet := CreateSpTunnelCreate(socksTunnel.Data)
+	ts.TsSyncAllClients(packet)
+
+	return nil
+}
+
+func (ts *Teamserver) TsTunnelStopSocks(AgentId string, Port int) {
+	port := strconv.Itoa(Port)
+	id := krypt.CRC32([]byte(AgentId + "socks5" + port))
+	TunnelId := fmt.Sprintf("%08x", id)
+
+	ts.TsTunnelStop(TunnelId)
 }
 
 /// Connection
