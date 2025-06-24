e8b298609a95d12443795a29bac82f512c728eba
Ralf <hackerralf8@gmail.com>
2025-06-24 23:20:58 +0300

    v0.6 update

---
+++ pre_install_linux_all.sh
@@ -0,0 +1,15 @@
+sudo apt update
+
+# server
+sudo apt install mingw-w64 make -y
+wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz -O /tmp/go1.24.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go /usr/local/bin/go
+sudo tar -C /usr/local -xzf /tmp/go1.24.4.linux-amd64.tar.gz
+sudo ln -s /usr/lib/go/bin/go /usr/local/bin/go
+
+# for windows 7 support by gopher agent
+git clone https://github.com/Adaptix-Framework/go-win7 /tmp/go-win7
+sudo mv /tmp/go-win7 /usr/lib/
+
+#client
+sudo apt install gcc g++ build-essential make cmake libssl-dev qt6-base-dev qt6-websockets-dev qt6-declarative-dev -y
