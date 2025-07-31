0dff029d5837eee1402d5a49d94e0cfeabeefd72
Ralf <54005327+RalfHacker@users.noreply.github.com>
2025-07-31 17:59:52 +0300

    Update pl_utils.go

--- Extenders/agent_beacon/pl_utils.go
+++ Extenders/agent_beacon/pl_utils.go
@@ -1173,7 +1173,7 @@ func parseDurationToSeconds(input string) (int, error) {
 func parseStringToWorkingTime(WorkingTime string) (int, error) {
 	IntWorkingTime := 0
 	if WorkingTime != "" {
-		match, err := regexp.MatchString("^[12]?[0-9]:[0-6][0-9]-[12]?[0-9]:[0-6][0-9]$", WorkingTime)
+		match, err := regexp.MatchString("^[012]?[0-9]:[0-6][0-9]-[012]?[0-9]:[0-6][0-9]$", WorkingTime)
 		if err != nil || match == false {
 			return IntWorkingTime, errors.New("Failed to parse working time: Invalid format")
 		}
